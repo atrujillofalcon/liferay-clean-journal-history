646f8cf471ce505d94fde4a43108c05a238e26bb
atrujillofalcon <atrujillo92work@gmail.com>
2016-12-01 16:54:45 +0100

    Added posibility of filter by site

--- script/cleanJournalArticleHistory.groovy
+++ script/cleanJournalArticleHistory.groovy
@@ -16,25 +16,37 @@ class CleanArticleHistory {
 
     void cleanHistory() throws Exception {
         long countDeleted = 0
-        int totalArticlesCount = JournalArticleLocalServiceUtil.getJournalArticlesCount()
+        int totalArticlesCount = 0
         List<Group> toAdd = new ArrayList<Group>()
-        toAdd.addAll(GroupLocalServiceUtil.getGroups(companyId, "com.liferay.portal.model.Company", 0))
-        toAdd.addAll(GroupLocalServiceUtil.getGroups(companyId, "com.liferay.portal.model.Group", 0))
+        if (groupIds.length > 0) {
+            for (long curGroupId : groupIds) {
+                Group curGroup = GroupLocalServiceUtil.fetchGroup(curGroupId)
+                if (curGroup != null) {
+                    toAdd.add(curGroup)
+                    totalArticlesCount += JournalArticleLocalServiceUtil.getArticlesCount(curGroupId)
+                }
+            }
+        } else {
+            totalArticlesCount = JournalArticleLocalServiceUtil.getJournalArticlesCount()
+            toAdd.addAll(GroupLocalServiceUtil.getGroups(10131, "com.liferay.portal.model.Company", 0))
+            toAdd.addAll(GroupLocalServiceUtil.getGroups(10131, "com.liferay.portal.model.Group", 0))
+        }
+
 
         for (Group curGroup : toAdd) {
-            _log.info("PROCESSING GROUP: " + curGroup.getGroupId())
+            _log.info("PROCESANDO GROUP: " + curGroup.getGroupId())
             DynamicQuery differentArticleIdsQuery = DynamicQueryFactoryUtil.forClass(JournalArticle.class, PortalClassLoaderUtil.getClassLoader())
             differentArticleIdsQuery.setProjection(ProjectionFactoryUtil.distinct(PropertyFactoryUtil.forName("articleId")))
             Criterion groupIdCriterion = RestrictionsFactoryUtil.eq("groupId", curGroup.getGroupId())
             differentArticleIdsQuery.add(groupIdCriterion)
             List<Object> differentIds = JournalArticleLocalServiceUtil.dynamicQuery(differentArticleIdsQuery)
             for (Object curId : differentIds) {
-                _log.info("PROCESSING ARTICLEID: " + curId)
+                _log.info("PROCESANDO ARTICLEID: " + curId)
                 JournalArticle lastVersion = JournalArticleLocalServiceUtil.fetchLatestArticle(curGroup.getGroupId(), (String) curId, WorkflowConstants.STATUS_APPROVED)
-                if (lastVersion == null){
-                    lastVersion=JournalArticleLocalServiceUtil.fetchLatestArticle(curGroup.getGroupId(), (String) curId, WorkflowConstants.STATUS_ANY)
-                    _log.warn("THIS JOURNAL ARTICLE HAS NOT ANY APPROVED VERSION")
-                    if(lastVersion==null) continue
+                if (lastVersion == null) {
+                    lastVersion = JournalArticleLocalServiceUtil.fetchLatestArticle(curGroup.getGroupId(), (String) curId, WorkflowConstants.STATUS_ANY)
+                    _log.warn("EL CONTENIDO NO TIENE NINGUNA VERSION APROBADA")
+                    if (lastVersion == null) continue
                 }
 
                 double lastVersionApproved = lastVersion.getVersion()
@@ -47,20 +59,22 @@ class CleanArticleHistory {
                         if (deleteArticles) {
                             JournalArticleLocalServiceUtil.deleteArticle(article)
                         }
-                        _log.debug("Deleted version: " + version + " of JournalArticle: " + curId)
+                        _log.debug("Borrada version: " + version + " del JournalArticle: " + curId)
                         versionsCount--; countDeleted++
                     }
                 }
-                _log.info("Current deleted: " + countDeleted + " from total: " + totalArticlesCount)
+                _log.info("Borrados: " + countDeleted + " de un total de: " + totalArticlesCount)
             }
         }
-        _log.info("DELETED AT THE SCRIPT END: " + countDeleted + " FROM TOTAL: " + totalArticlesCount)
+        _log.info("BORRADOS TOTALMENTE: " + countDeleted + " DE UN TOTAL DE: " + totalArticlesCount)
     }
 
     private static Log _log = LogFactoryUtil.getLog(CleanArticleHistory.class)
+    private static long[] groupIds = []
     private static boolean deleteArticles = true
     private static long companyId = CompanyThreadLocal.getCompanyId()
     private static long leaveVersionCount = 2
+    
     private static Comparator<JournalArticle> versionComparator = new Comparator<JournalArticle>() {
         @Override
         int compare(JournalArticle o1, JournalArticle o2) {
